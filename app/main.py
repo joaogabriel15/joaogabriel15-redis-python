b324c92482f688dd6a08e9c473a9a5f5abe99ecb
Pacheco Code <pacheco.code@gmail.com>
2024-05-24 00:19:15 -0300

    pass 1st stage

--- app/main.py
+++ app/main.py
@@ -11,7 +11,13 @@ def main():
     # server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     # server_socket.accept() # wait for client
     server_socket = socket.create_server(('localhost',6379), reuse_port=True)
-    server_socket.accept()
+    client, addr = server_socket.accept()
+
+    client.send(b"+PONG\r\n")
+   
+            
+
+    
 
 if __name__ == "__main__":
     main()
